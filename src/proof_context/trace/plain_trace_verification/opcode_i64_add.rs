19e53fe914972d4cc6a513d349bea09a5007f507
khaihanhtang <84257457+khaihanhtang@users.noreply.github.com>
2023-03-13 03:46:05 +0800

    Fix duplicate definitions by private mod
    Private mod for each check function, e.g.,  plainly_check_opcode_local_get, is introduced to handle private-part computation separately in order to avoid duplicate definitions, e.g., same function names specialized for different opcode checks.

--- src/proof_context/trace/plain_trace_verification/opcode_i64_add.rs
+++ src/proof_context/trace/plain_trace_verification/opcode_i64_add.rs
@@ -1,26 +1,42 @@
 use crate::proof_context::proof_context::ProofContext;
 use crate::proof_context::trace::state_trace_tuple::StateTraceTuple;
 
+mod private_part_for_plainly_checking_opcode_i64_add {
+    use crate::proof_context::proof_context::ProofContext;
+    use crate::proof_context::trace::state_trace_tuple::StateTraceTuple;
+
+    pub trait PrivatePart {
+        fn check_iaddr_and_pc_consistent(
+            current_state_trace_tuple: &StateTraceTuple,
+            next_state_trace_tuple: &StateTraceTuple,
+        );
+    }
+
+    impl PrivatePart for ProofContext {
+        fn check_iaddr_and_pc_consistent(
+            current_state_trace_tuple: &StateTraceTuple,
+            next_state_trace_tuple: &StateTraceTuple
+        ) {
+            assert_eq!(
+                current_state_trace_tuple.get_iaddr_before_executing() + 1, // since we need to keep param_index
+                next_state_trace_tuple.get_iaddr_before_executing()
+            );
+            assert_eq!(
+                current_state_trace_tuple.get_pc_before_executing() + 1,
+                next_state_trace_tuple.get_pc_before_executing()
+            );
+        }
+    }
+}
+
 impl ProofContext {
     pub fn plainly_check_opcode_i64_add(
         &self,
         current_state_trace_tuple: &StateTraceTuple,
         next_state_trace_tuple: &StateTraceTuple,
     ) {
-
-    }
-
-    fn check_iaddr_and_pc_consistent(
-        current_state_trace_tuple: &StateTraceTuple,
-        next_state_trace_tuple: &StateTraceTuple,
-    ) {
-        assert_eq!(
-            current_state_trace_tuple.get_iaddr_before_executing() + 1, // since we need to keep param_index
-            next_state_trace_tuple.get_iaddr_before_executing()
-        );
-        assert_eq!(
-            current_state_trace_tuple.get_pc_before_executing() + 1,
-            next_state_trace_tuple.get_pc_before_executing()
-        );
+        use self::private_part_for_plainly_checking_opcode_i64_add::PrivatePart;
+        Self::check_iaddr_and_pc_consistent(current_state_trace_tuple, next_state_trace_tuple);
+        todo!();
     }
 }
--- src/proof_context/trace/plain_trace_verification/opcode_local_get.rs
+++ src/proof_context/trace/plain_trace_verification/opcode_local_get.rs
@@ -8,12 +8,77 @@ use crate::proof_context::trace::ram_access_record::RamAccessRecord;
 
 const CONCATENATING_FACTOR: u64 = 256;
 
+mod private_part_for_plainly_checking_opcode_local_get {
+    use crate::proof_context::proof_context::ProofContext;
+    use crate::proof_context::trace::plain_trace_verification::check_constants;
+    use crate::proof_context::trace::proof_type::proof_access_type::ProofAccessType;
+    use crate::proof_context::trace::ram_access_record::RamAccessRecord;
+    use crate::proof_context::trace::state_trace_tuple::StateTraceTuple;
+
+    pub trait PrivatePart {
+        fn check_access_and_range_for_read_param_index(index_record: &RamAccessRecord);
+        fn check_access_and_range_for_read_byte(byte_record: &RamAccessRecord);
+        fn check_access_and_range_for_written_value(written_value_record: &RamAccessRecord);
+        fn check_iaddr_and_pc_consistent(
+            current_state_trace_tuple: &StateTraceTuple,
+            next_state_trace_tuple: &StateTraceTuple,
+        );
+    }
+
+    impl PrivatePart for ProofContext {
+        fn check_access_and_range_for_read_param_index(index_record: &RamAccessRecord) {
+            assert_eq!(index_record.get_access_type(), ProofAccessType::Read);
+            Self::verify_in_range(
+                0, check_constants::MAX_VALUE_64_BIT,
+                index_record.get_value()
+            );
+        }
+
+        fn check_access_and_range_for_read_byte(byte_record: &RamAccessRecord) {
+            assert_eq!(
+                byte_record.get_access_type(),
+                ProofAccessType::Read
+            );
+            Self::verify_in_range(
+                0, check_constants::MAX_VALUE_8_BIT,
+                byte_record.get_value()
+            );
+        }
+
+        fn check_access_and_range_for_written_value(written_value_record: &RamAccessRecord) {
+            assert_eq!(
+                written_value_record.get_access_type(),
+                ProofAccessType::Write
+            );
+            Self::verify_in_range(
+                0, check_constants::MAX_VALUE_64_BIT,
+                written_value_record.get_value()
+            );
+        }
+
+        fn check_iaddr_and_pc_consistent(
+            current_state_trace_tuple: &StateTraceTuple,
+            next_state_trace_tuple: &StateTraceTuple,
+        ) {
+            assert_eq!(
+                current_state_trace_tuple.get_iaddr_before_executing() + 2, // since we need to keep param_index
+                next_state_trace_tuple.get_iaddr_before_executing()
+            );
+            assert_eq!(
+                current_state_trace_tuple.get_pc_before_executing() + 1,
+                next_state_trace_tuple.get_pc_before_executing()
+            );
+        }
+    }
+}
+
 impl ProofContext {
     pub fn plainly_check_opcode_local_get(
         &self,
         current_state_trace_tuple: &StateTraceTuple,
         next_state_trace_tuple: &StateTraceTuple,
     ) {
+        use self::private_part_for_plainly_checking_opcode_local_get::PrivatePart;
         // check iaddr and pc
         Self::check_iaddr_and_pc_consistent(current_state_trace_tuple, next_state_trace_tuple);
 
@@ -50,47 +115,5 @@ impl ProofContext {
         assert_eq!(written_value, concatenated_value);
     }
 
-    fn check_access_and_range_for_read_param_index(index_record: &RamAccessRecord) {
-        assert_eq!(index_record.get_access_type(), ProofAccessType::Read);
-        Self::verify_in_range(
-            0, check_constants::MAX_VALUE_64_BIT,
-            index_record.get_value()
-        );
-    }
 
-    fn check_access_and_range_for_read_byte(byte_record: &RamAccessRecord) {
-        assert_eq!(
-            byte_record.get_access_type(),
-            ProofAccessType::Read
-        );
-        Self::verify_in_range(
-            0, check_constants::MAX_VALUE_8_BIT,
-            byte_record.get_value()
-        );
-    }
-
-    fn check_access_and_range_for_written_value(written_value_record: &RamAccessRecord) {
-        assert_eq!(
-            written_value_record.get_access_type(),
-            ProofAccessType::Write
-        );
-        Self::verify_in_range(
-            0, check_constants::MAX_VALUE_64_BIT,
-            written_value_record.get_value()
-        );
-    }
-
-    fn check_iaddr_and_pc_consistent(
-        current_state_trace_tuple: &StateTraceTuple,
-        next_state_trace_tuple: &StateTraceTuple,
-    ) {
-        assert_eq!(
-            current_state_trace_tuple.get_iaddr_before_executing() + 2, // since we need to keep param_index
-            next_state_trace_tuple.get_iaddr_before_executing()
-        );
-        assert_eq!(
-            current_state_trace_tuple.get_pc_before_executing() + 1,
-            next_state_trace_tuple.get_pc_before_executing()
-        );
-    }
 }
